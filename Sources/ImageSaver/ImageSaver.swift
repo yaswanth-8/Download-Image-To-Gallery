cab5e16a0e5beef85634df38820fc10d48325cfc
yaswanth <yaswanthm@presidio.com>
2023-11-09 17:29:15 +0530

    Update: Added alert call to the UI

--- Sources/ImageSaver/ImageSaver.swift
+++ Sources/ImageSaver/ImageSaver.swift
@@ -1,45 +1,58 @@
 import SwiftUI
 
-
 public class ImageSaver: NSObject {
-    public func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    public func writeToPhotoAlbum(image: UIImage, completion: @escaping (Error?) -> Void) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), completion)
     }
     
     @objc public func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save finished!")
+        if let error = error {
+            print("Save finished with error: \(error)")
+        } else {
+            print("Save finished successfully!")
+        }
     }
 }
 
 public struct DownloadImageToGalleryFromURLStringButton: View {
-    public var urlString : String?
+    public var urlString: String?
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+
     public init(urlString: String!) {
         self.urlString = urlString
     }
+
     public var body: some View {
         Button {
             let inputImageString: String = urlString ?? "https://ik.imagekit.io/hpapi/harry.jpg"
             guard let imageURL = URL(string: inputImageString) else { return }
-            
-            print("saved to gallery")
-            
+
             URLSession.shared.dataTask(with: imageURL) { data, response, error in
                 if let error = error {
-                    print("Error downloading image data: \(error)")
+                    alertMessage = "Error downloading image data: \(error)"
+                    showAlert = true
                     return
                 }
-                
+
                 if let data = data, let uiImage = UIImage(data: data) {
                     let imageSaver = ImageSaver()
-                    imageSaver.writeToPhotoAlbum(image: uiImage)
+                    imageSaver.writeToPhotoAlbum(image: uiImage) { saveError in
+                        if let saveError = saveError {
+                            alertMessage = "Error saving image to gallery: \(saveError.localizedDescription)"
+                        } else {
+                            alertMessage = "Image saved successfully!"
+                        }
+                        showAlert = true
+                    }
                 }
             }.resume()
+        } label: {
+            Image(systemName: "arrow.down.circle.fill")
+                .font(.system(size: 30))
         }
-        
-    label: {
-        Image(systemName: "arrow.down.circle.fill")
-            .offset(x:20)
-            .font(.system(size: 30))
-    }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Image Download"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+        }
     }
 }
